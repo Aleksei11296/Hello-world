2d921c48ce09636a635198e1e65853fced08cec4
Aleksei11296 <126506627+Aleksei11296@users.noreply.github.com>
2023-09-11 13:35:15 +0500

    Test Portal

---
+++ simpleapp/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+class Post(models.Model):
+    heading = models.CharField(
+        max_length=25,
+        unique=True,  # названия товаров не должны повторяться
+    )
+    text = models.TextField()
+    quantity = models.IntegerField(
+        validators=[MinValueValidator(0)],
+    )
+    # поле категории будет ссылаться на модель категории
+    category = models.ForeignKey(
+        to='Category',
+        on_delete=models.CASCADE,
+        related_name='post',  # все продукты в категории будут доступны через поле products
+    )
+    rating = models.IntegerField(
+        validators=[MinValueValidator(0)],
+    )
+
+    def __str__(self):
+        return f'{self.heading.title()}: {self.description[:20]}'
+
+
+# Категория, к которой будет привязываться товар
+class Category(models.Model):
+    # названия категорий тоже не должны повторяться
+    heading = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.heading.title()
+
+# Create your models here.
---
+++ simpleapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
+class PostList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = 'heading'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'post.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'post'
+
+# Create your views here.
