13fbb981d8bca5aa203eaee15baa838e06562815
Aleksei11296 <126506627+Aleksei11296@users.noreply.github.com>
2023-09-17 12:59:45 +0500

    Add files via upload

---
+++ inspectionProfiles/NewsPortal/NewsPortals/templatetags/custom_filters.py
@@ -0,0 +1,18 @@
+from django import template
+
+
+register = template.Library()
+
+
+# Регистрируем наш фильтр под именем currency, чтоб Django понимал,
+# что это именно фильтр для шаблонов, а не простая функция.
+@register.filter()
+def censor(value):
+   """
+   value: значение, к которому нужно применить фильтр
+   """
+   list = ['команда', 'дело', 'жизни']
+   for word in value.split():
+      if word in list:
+         value = value.replace(word, f"{word[0]}{'*' * (len(word) - 1)}")
+   return value
