81762a299b2a8ef631debaf4c1d06a04ab168ef4
Aleksei11296 <126506627+Aleksei11296@users.noreply.github.com>
2023-09-30 20:10:52 +0500

    Update signals.py

--- Hello-world12/NewsPortals/signals.py
+++ Hello-world12/NewsPortals/signals.py
@@ -35,4 +35,4 @@ def notify_about_new_post(sender, instance, **kwargs):
             subscribers = cat.subscribers.all()
             subscribers_emails += [s.email for s in subscribers]
 
-        send_notifications(instance.preview(), instance.pk(), instance.title, subscribers_emails)
+        send_notifications(instance.preview(), instance.pk, instance.heading, subscribers_emails)
