bc58ec79560ed5dea0ba3b23368a57c3bae3b632
Aleksei11296 <126506627+Aleksei11296@users.noreply.github.com>
2023-12-17 16:22:09 +0500

    Delete BulletinBoard directory

--- BulletinBoard/board/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post, CustomUser, Reply, Category
-
-admin.site.register(Post)
-admin.site.register(CustomUser)
-admin.site.register(Reply)
-admin.site.register(Category)
-# Register your models here.
